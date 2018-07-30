f3513d9bf21f932dc122e16459b61be380bfae3e
Omar <omar@Omars-MacBook-Air.local>
2018-07-30 21:37:36 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -203,7 +203,7 @@ $(function(){
 
 	function wrongCounter(){
 		$divs.fadeOut()
-		$container.html('<h1> You suck Tesfa and you eat Ass  </h1>')
+		$container.html('<h1> Woow that was fas... Oh you lost, Have another try ;)  </h1>')
 
 	};
 
